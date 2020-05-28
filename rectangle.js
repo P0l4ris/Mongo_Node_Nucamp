3123faa570249a25a094fec108ee0604cb6e0639
Luis Juarros <juarrosl0806@gmail.com>
2020-05-28 01:25:08 -0700

    Node callbacks and Enrror Handling

--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,19 @@
 //module.exports or simply exports make a JS file into a Node Module to export elsewhere in another JS file. these keywords are only used in NODE
-exports.perimeter = (x,y) => 2 * (x + y);
-exports.area = (x,y)  => x*y;
 
+//when not just a method use the whole model.exports
+//changed from two separate methods into one module function
+module.exports = (x,y, callback) => {
+    //error object (global info)as first argument
+    if ( x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+    } else {
+        setTimeout(() => 
+            //null means no error first. innerFunction in here, no need to pass x,y
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area:()  => x*y
+            }),
+            2000
+        );
+    }
+}
